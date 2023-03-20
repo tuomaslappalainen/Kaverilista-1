6565b7192f169d3b098fe7478ea99d1151839bf5
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2023-03-20 13:18:44 +0200

    Kaverilista 1 versio valmis

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+const kaverit = []
 
 
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#uusinimi input').value.length == 0){
         alert("Et voi telepaattisesti lisätä nimeä!")
     }
-    else{
+    else {
+
+      kaverit.push('#nimet'.innerHTML);
+      
+        if(kaverit.length > 10) {
+            alert("Maksimimäärä saavutettu")
+            return;
+        }
+
+
         document.querySelector('#nimet').innerHTML += `
             <div class="nimi">
                 <span id="nimi1">
